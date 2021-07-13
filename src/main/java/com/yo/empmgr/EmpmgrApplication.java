9065fadae56bddc3a666b52eb59976803af0da68
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2021-07-13 18:43:00 +0530

    Initial project

---
+++ src/main/java/com/yo/empmgr/EmpmgrApplication.java
@@ -0,0 +1,13 @@
+package com.yo.empmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpmgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yo/empmgr/EmpmgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yo.empmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpmgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
