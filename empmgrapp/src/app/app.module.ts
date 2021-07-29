d88d6e1634fae355791a04371a35faee12660eda
Yogesh Nikam <36443045+yogeshk04@users.noreply.github.com>
2021-07-29 08:37:39 +0530

    Front end updates

--- empmgrapp/src/app/app.module.ts
+++ empmgrapp/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- empmgrapp/src/app/employee.service.ts
+++ empmgrapp/src/app/employee.service.ts
@@ -4,27 +4,26 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Employee } from './employee';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class EmployeeService {
   private apiServerUrl= environment.apiBaseUrl;
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<Employee> {
-    return this.http.get<Employee>('${this.apiServerUrl}/employee/all');
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
   }
 
   public addEmployee(employee: Employee): Observable<Employee> {
-    return this.http.post<Employee>('${this.apiServerUrl}/employee/add', employee);
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
   }
 
   public updateEmployee(employee: Employee): Observable<Employee> {
-    return this.http.put<Employee>('${this.apiServerUrl}/employee/update', employee);
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
   }
 
   public deleteEmployee(id: number): Observable<void> {
-    return this.http.delete<void>('${this.apiServerUrl}/employee/delete/${id}');
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${id}`);
   }
 }
+ 
